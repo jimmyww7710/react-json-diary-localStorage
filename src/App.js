6947f75f3b9888be542ca3d696956ee1237b9e70
jimmyww7710 <36302780+jimmyww7710@users.noreply.github.com>
2025-01-01 22:06:48 +0800

    fix: == to ===

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ const App = () => {
         />
         <ul className="w-full mt-5 bg-white shadow rounded-lg divide-y divide-gray-200">
           {data.filter(item => {
-            return item?.content?.includes(filterText) || item?.date?.includes(filterText) || filterText == ""
+            return item?.content?.includes(filterText) || item?.date?.includes(filterText) || filterText === ""
           }).map((item) => (
             <li key={item.id} className="flex flex-wrap justify-between items-center p-4">
 
